dec96b3d5e34870be0b0272e636faa72218f2852
Ben Buchanan <benbuchanan23@gmail.com>
2022-03-20 19:59:09 -0400

    finished loop 1

---
+++ EXERCISES/Loops/1.rb
@@ -0,0 +1,10 @@
+loop do
+  puts 'Just keep printing...'
+  break
+end
+
+
+#Stopping a loop is as simple as adding the reserved word break 
+# inside the loop. break is nearly always necessary when using loop 
+# and doesn't require any conditions. When loop executes a break, it 
+# stops iterating immediately and exits the block.
