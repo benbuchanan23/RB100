1e6f47872a1d7f1747ee98623e60c35f2489f9a7
Ben Buchanan <benbuchanan23@gmail.com>
2022-03-27 12:39:19 -0400

    finished arrays exercises

---
+++ EXERCISES/Arrays/10.rb
@@ -0,0 +1,14 @@
+# Are We The Same? 
+  # In the code below, two arrays containing several numbers are assigned to two variables, array1 and array2.
+
+  array1 = [1, 5, 9]
+  array2 = [1, 9, 5]
+# Compare array1 and array2 and print true or false based on whether they match.
+
+puts array1 == array2
+
+if array1 == array2 
+  puts "These arrays are equal"
+else
+  puts "These arrays are different"
+end
