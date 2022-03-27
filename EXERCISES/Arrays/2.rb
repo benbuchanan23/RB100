b48685f786f6a069fe8f68bfbb0644108d86677b
Ben Buchanan <benbuchanan23@gmail.com>
2022-03-27 11:40:53 -0400

    finished exercises 1-3 Arrays

---
+++ EXERCISES/Arrays/2.rb
@@ -0,0 +1,12 @@
+# More Than One
+  # In the code below, an array containing different types of pets is assigned to pets.
+  # Write some code that selects 'fish' and 'lizard' from the pets array - select the two items at the same time. Assign the return value to a variable named my_pets, then print the contents of my_pets as a single string, e.g.:
+  
+  # Expected output: I have a pet fish and a pet lizard! 
+
+pets = ['cat', 'dog', 'fish', 'lizard']  
+
+my_pets = pets[2, 3]
+puts my_pets
+
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!" 
