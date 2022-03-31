86f774b77a20aaeb24b310c39343e5cbc7f9363f
Ben Buchanan <benbuchanan23@gmail.com>
2022-03-30 22:15:57 -0400

    continued revisions to Intro_to_Programming/Methods

---
+++ Intro_to_Programming/Methods/2.rb
@@ -0,0 +1,12 @@
+#  What do the following expressions evaluate to?
+
+
+  1. x = 2        
+  
+  2. puts x = 2
+  
+  3. p name = "Joe"
+  
+  4. four = "four"
+  
+  5. print something = "nothing"
---
+++ Intro_to_Programming/Methods/3.rb
@@ -0,0 +1,9 @@
+#  Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(2, 3)
+puts multiply(2.5, 4.5)
+puts multiply(0, 100000000)
